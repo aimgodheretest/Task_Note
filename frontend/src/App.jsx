1b2661d5d2c1fb72698a03c0511e8bfab7070fd0
Murli Kumar <aimgodhere@gmail.com>
2025-08-02 13:24:01 +0530

    change bg radial gradient color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import NoteDetailPage from "./pages/NoteDetailPage.jsx";
 const App = () => {
   return (
     <div className="relative h-full w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
@@ -18,3 +18,7 @@ const App = () => {
 };
 
 export default App;
+
+{
+  /* <div class=""></div>; */
+}
